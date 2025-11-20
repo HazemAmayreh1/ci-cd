9149de88d431c152d0f5c20e058e61ef5b33007a
HazemAmayreh <211099@ppu.edu.ps>
2025-11-20 17:50:10 +0200

    Setup CI/CD version test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD WORKING");
+  res.send("CI/CD WORKING test test ");
 });
 
 const PORT = process.env.PORT || 3000;
